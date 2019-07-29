31982a32ae2c0abc3705938535f5292555f9e320
Apple <apple@Apples-MacBook-Pro-68.local>
2019-07-29 18:03:12 -0500

    Stuff

---
+++ index.js
@@ -0,0 +1,24 @@
+var Q = $('.Q');
+var W = $('.W');
+var E = $('.E');
+var O = $('.O');
+var P = $('.P');
+var Bracket = $('.[');
+var A = $('.A');
+var S = $('.S');
+var D = $('.D');
+var K = $('.K');
+var L = $('.L');
+var Semi = $('.;');
+var Z = $('.Z');
+var X = $('.X');
+var C = $('.C');
+var N = $('.N');
+var M = $('.M');
+var Comma = $('.,');
+
+button.on("click",play);
+
+function play() {
+  var input = $('Q','W','E','O','P','[','A','S','D','K','L',';','Z','X','C','N','M',',')
+}
